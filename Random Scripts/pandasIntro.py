da36b2e549b8176381f6539f065a2e2bb0faf215
stephenJohnGiardina <stephengia@yahoo.com>
2020-07-03 14:21:04 -0400

    Practice with pandas file completed

---
+++ Random Scripts/pandasIntro.py
@@ -0,0 +1,57 @@
+dict = {"country": ["Brazil", "Russia", "India", "China", "South Africa"],
+       "capital": ["Brasilia", "Moscow", "New Dehli", "Beijing", "Pretoria"],
+       "area": [8.516, 17.10, 3.286, 9.597, 1.221],
+       "population": [200.4, 143.5, 1252, 1357, 52.98] }
+
+import pandas as pd
+print('==---==', dir(pd))
+brics = pd.DataFrame(dict)
+print(brics)
+
+# Set the index for brics
+brics.index = ["BR", "RU", "IN", "CH", "SA"]
+
+# Print out brics with new index values
+print(brics)
+
+# Import pandas as pd
+import pandas as pd
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('Random Scripts/cars.csv')
+
+# Print out cars
+print(cars)
+
+# Import pandas and cars.csv
+import pandas as pd
+cars = pd.read_csv('Random Scripts/cars.csv', index_col = 0)
+
+# Print out country column as Pandas Series
+print(cars['cars_per_cap'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['cars_per_cap']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['cars_per_cap', 'country']])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('Random Scripts/cars.csv', index_col = 0)
+
+# Print out first 4 observations
+print(cars[0:4])
+
+# Print out fifth and sixth observation
+print(cars[4:6])
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('Random Scripts/cars.csv', index_col = 1)
+
+# Print out observation for Japan
+print(cars.iloc[2])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
